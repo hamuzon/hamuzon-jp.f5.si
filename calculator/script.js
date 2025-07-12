84b0ffe6be856ff0bf7a6609029fb3752f60532c
hamuzon <nagasawa.1399@gmail.com>
2025-07-12 17:09:53 +0900

    v2.0 Code

---
+++ calculator/script.js
@@ -0,0 +1,15 @@
+function append(value) {
+  const display = document.getElementById('display');
+  display.value += value;
+}
+function clearDisplay() {
+  document.getElementById('display').value = '';
+}
+function calculate() {
+  const display = document.getElementById('display');
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = 'Error';
+  }
+}
