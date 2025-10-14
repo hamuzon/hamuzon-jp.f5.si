b793ade8c53396cbd6ca933af5f0097110e386ac
hamuzon <warabimoti@hamusata.f5.si>
2025-10-14 17:58:46 +0900

    v5.0.1 main.js

--- Device-info/main.js
+++ Device-info/main.js
@@ -289,6 +289,8 @@
       "hamuzon.github.io": { baseYear: 2025, user: "@hamuzon", link: "https://hamuzon.github.io" },
       "hamusata.f5.si": { baseYear: 2025, user: "@hamusata", link: "https://hamusata.f5.si" },
       "device-info.hamusata.f5.si": { baseYear: 2025, user: "@hamusata", link: "https://hamusata.f5.si" },
+      "device-info.hamuzon-jp.f5.si": { baseYear: 2025, user: "@hamuzon", link: "https://hamuzon.f5.si" },
+      "hamuzon-jp.f5.si": { baseYear: 2025, user: "@hamuzon", link: "https://hamuzon.f5.si" },
       "default": { baseYear: 2025, user: "device-info", link: "" }
     };
     const host = window.location.hostname;
