a37ba4fc42e2ec2247d7e1370e9043726bc56f6a
hamuzon <github@hamusata.f5.si>
2026-06-07 22:47:08 +0900

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,7 +1,6 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
 
-  // ドメイン末尾にドットがあるFQDN形式を正規化
   if (url.hostname.endsWith(".")) {
     url.hostname = url.hostname.slice(0, -1);
     return Response.redirect(url.toString(), 301);
